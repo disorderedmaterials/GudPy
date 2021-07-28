ea58d83521c0cf87a979081b71bf8ee308ec959e
Jared Swift <jared.swift@stfc.ac.uk>
2021-07-28 11:34:44 +0100

    Added test to compare percentage of DCS levels produced during a typical run, to those that we expected.

---
+++ tests/test_gudpy_workflows.py
@@ -0,0 +1,95 @@
+from gudrun_classes.gud_file import GudFile
+import os
+import sys
+from shutil import copyfile
+from unittest import TestCase
+import re
+
+try:
+    sys.path.insert(1, os.path.join(sys.path[0], "../gudrun_classes"))
+    from utils import spacify, numifyBool
+    from gudrun_file import GudrunFile
+
+except ModuleNotFoundError:
+    sys.path.insert(1, os.path.join(sys.path[0], "gudrun_classes"))
+    from gudrun_classes.gudrun_file import GudrunFile
+
+
+class TestGudPyWorkflows(TestCase):
+
+    def setUp(self) -> None:
+
+        path = "TestData/NIMROD-water/water.txt"
+
+        if os.name == "nt":
+            from pathlib import Path
+            dirpath = Path().resolve() / "tests/" / Path(path)
+        else:
+            dirpath = (
+                "/".join(os.path.realpath(__file__).split("/")[:-1])
+                + "/"
+                + path
+            )
+        self.g = GudrunFile(dirpath)
+
+        self.keepsakes = os.listdir()
+
+        copyfile(self.g.path, "tests/TestData/NIMROD-water/good_water.txt")
+        g = GudrunFile("tests/TestData/NIMROD-water/good_water.txt")
+
+        from pathlib import Path
+
+        parent = Path("tests").parent.absolute()
+        GudrunStartFolder = parent / "bin"
+        dataFileDir = Path("tests/TestData/NIMROD-water/raw").absolute()
+
+        g.instrument.GudrunStartFolder = GudrunStartFolder
+        g.instrument.dataFileDir = str(dataFileDir) + "/"
+        g.write_out(overwrite=True)
+
+        return super().setUp()
+
+    def tearDown(self) -> None:
+
+        if os.path.isfile("test_data.txt"):
+            os.remove("test_data.txt")
+        if os.path.isfile("tests/TestData/NIMROD-water/good_water.txt"):
+            os.remove("tests/TestData/NIMROD-water/good_water.txt")
+
+        for f in os.listdir():
+            if f not in self.keepsakes:
+                os.remove(f)
+
+        return super().tearDown()
+
+    def testGudPyDCS(self):
+        
+        self.g.dcs()
+        
+        gfPath = self.g.sampleBackgrounds[0].samples[0].dataFiles.dataFiles[0]
+        gfPath = gfPath.replace(self.g.instrument.dataFileType, 'gud')
+        gf1 = GudFile(gfPath)
+        dcsLevelPercentage = re.findall('\d*[.]?\d*%', gf1.err)[0]
+        dcsLevelPercentage = float(dcsLevelPercentage.replace('%',''))
+        self.assertAlmostEqual(dcsLevelPercentage, 14.4, 0)
+
+        gfPath = self.g.sampleBackgrounds[0].samples[1].dataFiles.dataFiles[0]
+        gfPath = gfPath.replace(self.g.instrument.dataFileType, 'gud')
+        gf2 = GudFile(gfPath)
+        dcsLevelPercentage = re.findall('\d*[.]?\d*%', gf2.result)[0]
+        dcsLevelPercentage = float(dcsLevelPercentage.replace('%',''))
+        self.assertAlmostEqual(dcsLevelPercentage, 100.0, 0)
+
+        gfPath = self.g.sampleBackgrounds[0].samples[2].dataFiles.dataFiles[0]
+        gfPath = gfPath.replace(self.g.instrument.dataFileType, 'gud')
+        gf3 = GudFile(gfPath)
+        dcsLevelPercentage = re.findall('\d*[.]?\d*%', gf3.result)[0]
+        dcsLevelPercentage = float(dcsLevelPercentage.replace('%',''))
+        self.assertAlmostEqual(dcsLevelPercentage, 98.0, 0)
+
+        gfPath = self.g.sampleBackgrounds[0].samples[3].dataFiles.dataFiles[0]
+        gfPath = gfPath.replace(self.g.instrument.dataFileType, 'gud')
+        gf4 = GudFile(gfPath)
+        dcsLevelPercentage = re.findall('\d*[.]?\d*%', gf4.err)[0]
+        dcsLevelPercentage = float(dcsLevelPercentage.replace('%',''))
+        self.assertAlmostEqual(dcsLevelPercentage, 13.0, 0)
