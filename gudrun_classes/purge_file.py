737419374628100fadda8fa36843a2a659341a9a
Jared Swift <jared.swift@stfc.ac.uk>
2021-07-29 10:23:19 +0100

    PEP8 formatting.

--- gudrun_classes/purge_file.py
+++ gudrun_classes/purge_file.py
@@ -8,16 +8,23 @@ except ModuleNotFoundError:
     sys.path.insert(1, os.path.join(sys.path[0], "scripts"))
     from scripts.utils import spacify, numifyBool
 
+
 class PurgeFile():
 
-    def __init__(self, gudrunFile, standardDeviation=(10.0, 10.0), ignoreBad=True):
-        
+    def __init__(
+            self, gudrunFile, standardDeviation=(10.0, 10.0), ignoreBad=True
+                ):
+
         self.gudrunFile = gudrunFile
         self.instrumentName = gudrunFile.instrument.name
         self.inputFileDir = gudrunFile.instrument.GudrunInputFileDir
         self.dataFileDir = gudrunFile.instrument.dataFileDir
-        self.detCalibFile = os.path.abspath(gudrunFile.instrument.detectorCalibrationFileName)
-        self.groupsFile = os.path.abspath(gudrunFile.instrument.groupFileName)
+        self.detCalibFile = (
+            os.path.abspath(gudrunFile.instrument.detectorCalibrationFileName)
+        )
+        self.groupsFile = (
+            os.path.abspath(gudrunFile.instrument.groupFileName)
+        )
         self.spectrumNumbers = (
             gudrunFile.instrument.spectrumNumbersForIncidentBeamMonitor
         )
@@ -28,7 +35,7 @@ class PurgeFile():
             gudrunFile.instrument.spikeAnalysisAcceptanceFactor
         )
         self.standardDeviation = standardDeviation
-        self.ignoreBad  = ignoreBad
+        self.ignoreBad = ignoreBad
         self.normalisationPeriodNo = (
             self.gudrunFile.normalisation.numberOfFilesPeriodNumber[1]
         )
@@ -38,19 +45,19 @@ class PurgeFile():
         for sampleBackground in self.gudrunFile.sampleBackgrounds:
             periodNumber = sampleBackground.numberOfFilesPeriodNumber[1]
             for dataFile in sampleBackground.dataFiles.dataFiles:
-                self.sampleBackgroundDataFiles+= (
+                self.sampleBackgroundDataFiles += (
                     dataFile + "  " + str(periodNumber) + "\n"
                 )
             for sample in sampleBackground.samples:
                 periodNumber = sample.numberOfFilesPeriodNumber[1]
                 for dataFile in sample.dataFiles.dataFiles:
-                    self.sampleDataFiles+= (
+                    self.sampleDataFiles += (
                         dataFile + "  " + str(periodNumber) + "\n"
                     )
                 for container in sample.containers:
                     periodNumber = container.numberOfFilesPeriodNumber[1]
                     for dataFile in container.dataFiles.dataFiles:
-                        self.containerDataFiles+= (
+                        self.containerDataFiles += (
                             dataFile + "  " + str(periodNumber) + "\n"
                         )
 
@@ -59,7 +66,7 @@ class PurgeFile():
         f = open("purge_det.dat", "w", encoding="utf-8")
         f.write(str(self))
         f.close()
-    
+
     def __str__(self):
 
         HEADER = "'  '  '          '  '/'\n\n"
@@ -92,12 +99,14 @@ class PurgeFile():
             f'{self.sampleDataFiles}'
             f'{self.containerDataFiles}'
         )
-        
+
     def purge(self):
         self.write_out()
         try:
             result = subprocess.run(
-                ["bin/purge_det", "purge_det.dat"], capture_output=True, text=True
+                ["bin/purge_det", "purge_det.dat"],
+                capture_output=True,
+                text=True
             )
         except FileNotFoundError:
             purge_det = sys._MEIPASS + os.sep + "purge_det.dat"
