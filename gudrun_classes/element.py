1b5aabe221d5c74470c02cd60871f760c06b7508
Jared Swift <jared.swift@stfc.ac.uk>
2021-08-04 14:46:37 +0100

    Docstrings for Element

--- gudrun_classes/element.py
+++ gudrun_classes/element.py
@@ -1,10 +1,51 @@
 class Element:
+    """
+    Class to represent an Element.
+
+    ...
+
+    Attributes
+    ----------
+    atomicSymbol : str
+        The atomic symbol belonging to the element, e.g. H.
+    massNo : int
+        The atomic number belonging to the element (total number of nucleons).
+    abundance : float
+        Abundance of the element.
+    Methods
+    -------
+    """
     def __init__(self, atomicSymbol, massNo, abundance):
+        """
+        Constructs all the necessary attributes for the DataFiles object.
+
+        Parameters
+        ----------
+        atomicSymbol : str
+            The atomic symbol belonging to the element, e.g. H.
+        massNo : int
+            The atomic number belonging to the element
+            (total number of nucleons).
+        abundance : float
+            Abundance of the element.
+        """
         self.atomicSymbol = atomicSymbol
         self.massNo = massNo
         self.abundance = abundance
 
     def __str__(self):
+        """
+        Returns the string representation of the Element object.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        string : str
+            String representation of Element.
+        """
         return (
             self.atomicSymbol
             + "  "
@@ -14,4 +55,16 @@ class Element:
         )
 
     def __repr__(self):
+        """
+        Returns the string representation of the Element object.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        string : str
+            String representation of Element.
+        """
         return str(self)
