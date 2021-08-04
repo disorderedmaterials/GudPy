22220d296dc7a2b5c418fc1965793b70bfe51215
Jared Swift <jared.swift@stfc.ac.uk>
2021-08-04 17:49:21 +0100

    Docstrings for SampleBackground.

--- gudrun_classes/sample_background.py
+++ gudrun_classes/sample_background.py
@@ -7,12 +7,48 @@ except ModuleNotFoundError:
 
 
 class SampleBackground:
+    """
+    Class to represent a SampleBackground.
+
+    ...
+
+    Attributes
+    ----------
+    numberOfFilesPeriodNumber : tuple(int, int)
+        Number of data files and their period number.
+    dataFiles : DataFiles
+        DataFiles object storing data files belonging to the container.
+    samples : Sample[]
+        List of Sample objects against the SampleBackground.
+    Methods
+    -------
+    """
     def __init__(self):
+        """
+        Constructs all the necessary attributes for the
+        SampleBackground object.
+
+        Parameters
+        ----------
+        None
+        """
         self.numberOfFilesPeriodNumber = (0, 0)
         self.dataFiles = DataFiles([], "SAMPLE BACKGROUND")
         self.samples = []
 
     def __str__(self):
+        """
+        Returns the string representation of the SampleBackground object.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        string : str
+            String representation of SampleBackground.
+        """
         TAB = "          "
         SAMPLES = "\n".join([str(x) for x in self.samples if x.runThisSample])
 
