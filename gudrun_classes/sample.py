aa9d270a6dd6d1b24ab2c255d4f35715ebbc1fa9
Jared Swift <87013792+jswift-stfc@users.noreply.github.com>
2021-08-08 17:58:06 +0100

    Fix semantic error in Sample.

--- gudrun_classes/sample.py
+++ gudrun_classes/sample.py
@@ -144,7 +144,7 @@ class Sample:
         if self.densityUnits == UnitsOfDensity.ATOMIC:
             units = 'atoms/\u212b^3'
             density = self.density*-1
-        elif self.density == UnitsOfDensity.CHEMICAL:
+        elif self.densityUnits == UnitsOfDensity.CHEMICAL:
             units = 'gm/cm^3'
             density = self.density
 
