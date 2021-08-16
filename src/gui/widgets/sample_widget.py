5c4a1f2406dc670533456b2478b8464108422a11
Jared Swift <jared.swift@stfc.ac.uk>
2021-08-16 10:38:17 +0100

    Update sample widget.

--- src/gui/widgets/sample_widget.py
+++ src/gui/widgets/sample_widget.py
@@ -1,4 +1,4 @@
-from gudrun_classes.enums import Geometry, UnitsOfDensity
+from gudrun_classes.enums import Geometry, NormalisationType, OutputUnits, UnitsOfDensity
 from PyQt5.QtWidgets import QTableWidgetItem, QWidget, QHeaderView
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
@@ -31,15 +31,49 @@ class SampleWidget(QWidget):
 
         self.geometryComboBox.addItems([g.name for g in Geometry])
         self.geometryComboBox.setCurrentIndex(self.sample.geometry.value)
+        print(self.sample.geometry.value)
         self.upstreamLineEdit.setText(str(self.sample.thickness[0]))
         self.downstreamLineEdit.setText(str(self.sample.thickness[1]))
 
-        # angle of rotation an sample width
+
 
         self.densityLineEdit.setText(str(self.sample.density))
         self.densityUnitsComboBox.addItems([du.name for du in UnitsOfDensity])
         self.densityUnitsComboBox.setCurrentIndex(self.sample.densityUnits.value)
         
-        #temp for normalisation pc
 
+        crossSectionSources = ["TABLES", "TRANSMISSION MONITOR", "FILENAME"]
+        if "TABLES" in self.sample.totalCrossSectionSource:
+            index = 0
+        elif "TRANSMISSION" in self.sample.totalCrossSectionSource:
+            index = 1
+        else:
+            index = 2
+        self.totalCrossSectionComboBox.addItems(crossSectionSources)
+        self.totalCrossSectionComboBox.setCurrentIndex(index)
 
+        self.tweakFactorLineEdit.setText(str(self.sample.sampleTweakFactor))
+
+        self.topHatWidthLineEdit.setText(str(self.sample.topHatW))
+        self.minLineEdit.setText(str(self.sample.minRadFT))
+        self.maxLineEdit.setText(str(self.sample.maxRadFT))
+
+        self.broadeningFunctionLineEdit.setText(str(self.sample.grBroadening))
+        self.broadeningPowerLineEdit.setText(str(self.sample.powerForBroadening))
+
+        self.stepSizeLineEdit.setText(str(self.sample.stepSize))
+
+        self.scatteringFileLineEdit.setText(self.sample.fileSelfScattering)
+
+        self.normaliseToComboBox.addItems([n.name for n in NormalisationType])
+        self.normaliseToComboBox.setCurrentIndex(self.sample.normaliseTo.value)
+
+        self.outputUnitsComboBox.addItems([u.name for u in OutputUnits])
+        self.outputUnitsComboBox.setCurrentIndex(self.sample.outputUnits.value)
+
+        self.angleOfRotationLineEdit.setText(str(self.sample.angleOfRotationSampleWidth[0]))
+        self.sampleWidthLineEdit.setText(str(self.sample.angleOfRotationSampleWidth[1]))
+
+        self.correctionFactorLineEdit.setText(str(self.sample.normalisationCorrectionFactor))
+        self.scatteringFractionLineEdit.setText(str(self.sample.environementScatteringFuncAttenuationCoeff[0]))
+        self.attenuationCoefficientLineEdit.setText(str(self.sample.environementScatteringFuncAttenuationCoeff[1]))
