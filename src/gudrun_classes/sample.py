50ba6216ee3317854e631327a1b2bffe596156cc
Jared Swift <87013792+jswift-stfc@users.noreply.github.com>
2021-08-17 14:24:14 +0100

    Update sample.py

--- src/gudrun_classes/sample.py
+++ src/gudrun_classes/sample.py
@@ -2,7 +2,9 @@ from src.gudrun_classes import pathmagic  # noqa: F401
 from src.scripts.utils import spacify, numifyBool
 from src.gudrun_classes.data_files import DataFiles
 from src.gudrun_classes.composition import Composition
-from src.gudrun_classes.enums import UnitsOfDensity, NormalisationType, OutputUnits, Geometry
+from src.gudrun_classes.enums import (
+    UnitsOfDensity, NormalisationType, OutputUnits, Geometry
+)
 
 
 class Sample:
