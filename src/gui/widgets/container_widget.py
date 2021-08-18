86770cc89f7fb0aef1e14fa37ef9ad29c7355960
Jared Swift <jared.swift@stfc.ac.uk>
2021-08-18 15:42:30 +0100

    Docstrings for ContainerWidget.

--- src/gui/widgets/container_widget.py
+++ src/gui/widgets/container_widget.py
@@ -3,7 +3,33 @@ from PyQt5 import uic
 import os
 
 class ContainerWidget(QWidget):
+    """
+    Class to represent a ContainerWidget. Inherits QWidget.
+
+    ...
+
+    Attributes
+    ----------
+    container : Container
+        Container object belonging to the GudrunFile.
+    parent : QWidget
+        Parent widget.
+    Methods
+    -------
+    initComponents()
+        Loads UI file, and then populates data from the Container.
+    """
     def __init__(self, container, parent=None):
+        """
+        Constructs all the necessary attributes for the ContainerWidget object.
+        Calls the initComponents method, to load the UI file and populate data.
+        Parameters
+        ----------
+        container : Container
+            Container object belonging to the GudrunFile.
+        parent : QWidget
+            Parent widget.
+        """
         self.container = container
         self.parent = parent
 
@@ -11,6 +37,21 @@ class ContainerWidget(QWidget):
         self.initComponents()
     
     def initComponents(self):
+        """
+        Loads the UI file for the ContainerWidget object,
+        and then populates the child widgets with their
+        corresponding data from the attributes of the Container object.
+        ----------
+        container : Container
+            Container object belonging to the GudrunFile.
+        parent : QWidget
+            Parent widget.
+        """
+
+        # Get the current directory that we are residing in.
         current_dir = os.path.dirname(os.path.realpath(__file__))
+        # Join the current directory with the relative path of the UI file.
         uifile = os.path.join(current_dir, "ui_files/containerWidget.ui")
+        
+        # Use pyuic to load to the UI file into the ContainerWidget.
         uic.loadUi(uifile, self)
