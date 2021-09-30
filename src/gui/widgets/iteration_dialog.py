aeeca31078193db8c7a7f9fc02f4268c34629b59
Jared Swift <jared.swift@stfc.ac.uk>
2021-09-30 11:05:05 +0100

    feat: completed minimal iteration dialog.

--- src/gui/widgets/iteration_dialog.py
+++ src/gui/widgets/iteration_dialog.py
@@ -1,15 +1,49 @@
 from PyQt5.QtWidgets import QDialog
 from PyQt5 import uic
-from PyQt5.QtCore import Qt
 import os
 from enum import Enum
 from src.gudrun_classes.tweak_factor_iterator import TweakFactorIterator
-from src.gudrun_classes.wavelength_subtraction_iterator import WavelengthSubtractionIterator
+from src.gudrun_classes.wavelength_subtraction_iterator import (
+    WavelengthSubtractionIterator
+)
+
+
 class Tweakables(Enum):
     TWEAK_FACTOR = 0
 
-class IterationDialog(QDialog):
 
+class IterationDialog(QDialog):
+    """
+    Class to represent the IterationDialog. Inherits QDialog.
+    This is the dialog window opened when a user wishes to iterate Gudrun.
+
+    ...
+
+    Attributes
+    ----------
+    gudrunFile : GudrunFile
+        GudrunFile object currently associated with the application.
+    tweakValues: bool
+        Should iteration by tweaking be performed?
+    tweak : Tweakables
+        Attribute to tweak by.
+    performInelasticitySubtractions : bool
+        Should iteration by inelasticity subtractions be performed?
+    numberIterations : int
+        Number of iterations to perform.
+    Methods
+    -------
+    initComponents()
+        Loads the UI file for the IterationDialog
+    handleTweakValuesChanged(state)
+        Slot for handling toggling iteration by tweaking.
+    handlePerformInelasticitySubtractionsChanged(state)
+        Slot for handling toggling performing inelasticity subtractions.
+    handleNumberIterationsChanged(value)
+        Slot for handling change in the number of iterations.
+    iterate()
+        Iterate Gudrun with the specified configuration.
+    """
     def __init__(self, gudrunFile, parent):
         super(IterationDialog, self).__init__(parent=parent)
         self.gudrunFile = gudrunFile
@@ -20,16 +54,51 @@ class IterationDialog(QDialog):
         self.numberIterations = 0
 
     def handleTweakValuesChanged(self, state):
+        """
+        Slot for handling toggling iteration by tweaking.
+        Caled when a toggled signal is emitted from the tweakButton.
+        Updates the iteration configuration as such, and toggles the
+        visibility of the tweakWidget as such.
+        Parameters
+        ----------
+        state : int
+            The new state of the tweakButton (1: True, 0: False).
+        """
         self.tweakValues = state
         self.tweakWidget.setVisible(state)
 
     def handlePerformInelasticitySubtractionsChanged(self, state):
+        """
+        Slot for handling toggling performing inelasticity subtractions.
+        Caled when a toggled signal is emitted from the
+        inelasticitySubtractionsButton.
+        Updates the iteration configuration as such.
+        Parameters
+        ----------
+        state : int
+            The new state of the inelasticitySubtractionsButton
+            (1: True, 0: False).
+        """
         self.performInelasticitySubtractions = state
 
     def handleNumberIterationsChanged(self, value):
+        """
+        Slot for handling change in the number of iterations.
+        Caled when a valueChanged signal is emitted from the
+        noIterationsSpinBox.
+        Updates the iteration configuration as such.
+        Parameters
+        ----------
+        value : int
+            The new value of the noIterationsSpinBox.
+        """
         self.numberIterations = value
 
     def iterate(self):
+        """
+        Iterate Gudrun with the specified configuration.
+        Called when an accepted signal is emmited from the buttonBox.
+        """
         if self.tweakValues:
             if self.tweak == Tweakables.TWEAK_FACTOR:
                 tweakFactorIterator = TweakFactorIterator(self.gudrunFile)
@@ -37,14 +106,16 @@ class IterationDialog(QDialog):
             else:
                 pass
         elif self.performInelasticitySubtractions:
-            wavelengthSubtractionIterator = WavelengthSubtractionIterator(self.gudrunFile)
+            wavelengthSubtractionIterator = WavelengthSubtractionIterator(
+                self.gudrunFile
+            )
             wavelengthSubtractionIterator.iterate(self.numberIterations)
         else:
             pass
 
     def initComponents(self):
         """
-        Loads the UI file for the SampleWidget object.
+        Loads the UI file for the IterationDialog object.
         """
         current_dir = os.path.dirname(os.path.realpath(__file__))
         uifile = os.path.join(current_dir, "ui_files/iterationDialog.ui")
