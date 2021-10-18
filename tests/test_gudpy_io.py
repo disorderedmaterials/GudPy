cccf94108ccdbf80bfaf23744471cbfba7cb023d
Jared Swift <jared.swift@stfc.ac.uk>
2021-10-18 16:41:52 +0100

    fix: syntax error.

--- tests/test_gudpy_io.py
+++ tests/test_gudpy_io.py
@@ -79,7 +79,7 @@ class TestGudPyIO(TestCase):
             "scaleSelection": Scales.Q,
             "subWavelengthBinnedData": 0,
             "GudrunStartFolder": "bin",
-            "startupFileFolder": "StartupFiles"
+            "startupFileFolder": "StartupFiles",
             "logarithmicStepSize": 0.04,
             "hardGroupEdges": True,
             "nxsDefinitionFile": "",
