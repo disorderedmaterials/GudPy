57e8329086a08630175e531f9b1120e66e91c296
Jared Swift <jared.swift@stfc.ac.uk>
2021-10-25 10:22:37 +0100

    tests: tests for GroupingParameterModel.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from PySide6.QtCore import QKeyCombination, QModelIndex, Qt
 
-from src.gui.widgets.gudpy_tables import GudPyTableModel
+from src.gui.widgets.gudpy_tables import GroupingParameterModel, GudPyTableModel
 
 class TestModels(TestCase):
 
@@ -34,7 +34,33 @@ class TestModels(TestCase):
         self.assertEqual(model.rowCount(QModelIndex()), 0)
     
     def testGroupingParameterModel(self):
-        pass
+        
+        model = GroupingParameterModel([[0, 0., 0., 0.]], ["Group", "XMin", "XMax", "Background Factor"], None)
+
+        self.assertEqual(model._data, [[0, 0., 0., 0.]])
+        self.assertEqual(model.headers, ["Group", "XMin", "XMax", "Background Factor"])
+        self.assertEqual(model.parent(), None)
+        self.assertEqual(model.rowCount(QModelIndex()), 1)
+        self.assertEqual(model.columnCount(QModelIndex()), 4)
+
+        self.assertEqual(model.data(model.index(0, 0, QModelIndex()), Qt.EditRole), 0)
+        self.assertEqual(model.data(model.index(0, 1, QModelIndex()), Qt.EditRole), 0.)
+        self.assertEqual(model.data(model.index(0, 2, QModelIndex()), Qt.EditRole), 0.)
+        self.assertEqual(model.data(model.index(0, 3, QModelIndex()), Qt.EditRole), 0.)
+
+        self.assertEqual(model.headerData(0, Qt.Horizontal, Qt.DisplayRole), "Group")
+        self.assertEqual(model.headerData(1, Qt.Horizontal, Qt.DisplayRole), "XMin")
+        self.assertEqual(model.headerData(2, Qt.Horizontal, Qt.DisplayRole), "XMax")
+        self.assertEqual(model.headerData(3, Qt.Horizontal, Qt.DisplayRole), "Background Factor")
+
+        model.insertRow()
+        self.assertEqual(model.data(model.index(1, 0, QModelIndex()), Qt.EditRole), 0)
+        self.assertEqual(model.data(model.index(1, 1, QModelIndex()), Qt.EditRole), 0.)
+        self.assertEqual(model.data(model.index(1, 2, QModelIndex()), Qt.EditRole), 0.)
+        self.assertEqual(model.data(model.index(1, 3, QModelIndex()), Qt.EditRole), 0.)
+
+        model.setData(model.index(0, 3, QModelIndex()), 1., Qt.EditRole)
+        self.assertEqual(model.data(model.index(0, 3, QModelIndex()), Qt.EditRole), 1.)
 
     def testBeamProfileModel(self):
         pass
