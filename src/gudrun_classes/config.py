ba4f0311af47d1a103176aa9b3f4831ac2a3ca75
Jared Swift <jared.swift@stfc.ac.uk>
2022-02-01 16:23:14 +0000

    feat: store dict of container configurations in config.

--- src/gudrun_classes/config.py
+++ src/gudrun_classes/config.py
@@ -1,8 +1,21 @@
 from src.gudrun_classes.composition import Components
 from src.gudrun_classes.enums import Geometry
+import os
+import sys
 
 geometry = Geometry.FLATPLATE
 NUM_GUDPY_CORE_OBJECTS = 4
 USE_USER_DEFINED_COMPONENTS = False
 NORMALISE_COMPOSITIONS = False
 components = Components()
+
+containerConfigurations = {
+    path
+    .replace(".config", "")
+    .replace("_", " ") : 
+    path for path in os.listdir(
+        os.path.join(sys._MEIPASS, "bin", "configs", "containers")
+        if hasattr(sys, "_MEIPASS")
+        else os.path.join("bin", "configs", "containers")
+    )
+}
