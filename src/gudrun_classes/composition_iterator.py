6deb34537b439bc91db99c2590988dff1b2d071c
Jared Swift <jared.swift@stfc.ac.uk>
2022-02-11 10:52:02 +0000

    feat: comprehensive iteration by single component, using GSS

--- src/gudrun_classes/composition_iterator.py
+++ src/gudrun_classes/composition_iterator.py
@@ -1,21 +1,16 @@
+from copy import deepcopy
 from src.gudrun_classes.gud_file import GudFile
 import os
 import time
 from scipy.optimize import minimize, minimize_scalar
 import sys
 import numpy as np
+
+
+
 class CompositionIterator():
     def __init__(self, gudrunFile):
-        """
-        Constructs all the necessary attributes for the
-        SingleParamIterator object.
-
-        Parameters
-        ----------
-        gudrunFile : GudrunFile
-            Input GudrunFile that we will be using for iterating.
-        """
-        self.gudrunFile = gudrunFile
+        self.gudrunFile = deepcopy(gudrunFile)
         self.components = []
         self.ratio = 0
 
@@ -23,48 +18,48 @@ class CompositionIterator():
         self.components = [component]
         self.ratio = ratio
     
-    def setComponents(self, components, ratio=1):
+    def setComponents(self, components, ratioX=1, ratioY=1):
         self.components = components
-        self.ratio = ratio
+        self.ratioX = ratioX
+        self.ratioY = ratioY
 
-    def costFunction(self, x):
-        if isinstance(x, (list, np.ndarray)):
-            x = x[0]
+    def process(self, x, sampleBackground):
+        gudrunFile = deepcopy(self.gudrunFile)
+        gudrunFile.sampleBackgrounds = [sampleBackground]
+
         x = abs(x)
-        print(x)
-        sys.stdout.flush()
-        if len(self.components) == 1:
-            for sample in [s for s in self.gudrunFile.sampleBackgrounds[0].samples if s.runThisSample]:
-                weightedComponents = [wc for wc in sample.composition.weightedComponents for c in self.components if c == wc.component]
-                for component in weightedComponents:
-                    component.ratio = x
-                sample.composition.translate()
-            (self.gudrunFile.process())
-            time.sleep(1)
-            for sampleBackground in self.gudrunFile.sampleBackgrounds:
-                for sample in [s for s in sampleBackground.samples if s.runThisSample]:
-                        gudPath = sample.dataFiles.dataFiles[0].replace(
-                                    self.gudrunFile.instrument.dataFileType,
-                                    "gud"
-                                )
-                        gudFile = GudFile(
-                            os.path.join(
-                                self.gudrunFile.instrument.GudrunInputFileDir, gudPath
-                            )
-                        )
-                        print(gudFile.averageLevelMergedDCS, gudFile.expectedDCS, (gudFile.expectedDCS-gudFile.averageLevelMergedDCS)**2)
-                        if gudFile.averageLevelMergedDCS == gudFile.expectedDCS:
-                            return 0
-                        else:
-                            return (gudFile.expectedDCS-gudFile.averageLevelMergedDCS)**2
-    def iterate(self, n):
+        weightedComponents = [wc for wc in sampleBackground.samples[0].composition.weightedComponents for c in self.components if c == wc.component]
+        for component in weightedComponents:
+            component.ratio = x
+            print(component.ratio)
 
-        # Nelder-Mead
-        result = minimize(self.costFunction, x0=self.ratio, tol=1e-4, method='Nelder-Mead', options={"maxiter":n, "disp":True})
-        print(f"final ratio: {result['x']}")
-        print(f"Success: {result['success']}")
+        sampleBackground.samples[0].composition.translate()
+        gudrunFile.process()
 
-        # Golden Section Sort
-        result = minimize_scalar(self.costFunction, method='Golden', options={"maxiter":n, "disp":2, "xtol": 1})
-        print(f"final ratio: {result['x']}")
-        print(f"Success: {result['success']}")
+        time.sleep(1)
+        gudPath = sampleBackground.samples[0].dataFiles.dataFiles[0].replace(
+                    gudrunFile.instrument.dataFileType,
+                    "gud"
+                )
+        gudFile = GudFile(
+            os.path.join(
+                gudrunFile.instrument.GudrunInputFileDir, gudPath
+            )
+        )
+
+        print(gudFile.averageLevelMergedDCS, gudFile.expectedDCS, (gudFile.expectedDCS-gudFile.averageLevelMergedDCS)**2)
+        if gudFile.averageLevelMergedDCS == gudFile.expectedDCS:
+            return 0
+        else:
+            return (gudFile.expectedDCS-gudFile.averageLevelMergedDCS)**2
+
+    def iterate(self, n=5, rtol=10):
+        if not self.components or not self.ratio: return None
+
+        for sampleBackground in self.gudrunFile.sampleBackgrounds:
+            for sample in sampleBackground.samples:
+                if sample.runThisSample:
+                    sb = deepcopy(sampleBackground)
+                    sb.samples = [sample]
+                    result = minimize_scalar(self.process, args=(sb,), method='Golden', options={"maxiter":n, "xtol": rtol})
+                    print(f"final ratio for component {self.components[0].name} in {sample.name}: {result['x']}")
