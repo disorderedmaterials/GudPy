355bded49e702c9fcfcf382b93b2c2ab0506a39f
jswift-stfc <jared.swift@stfc.ac.uk>
2022-04-01 15:36:28 +0100

    feat: stubs for iteration dialogs.

---
+++ src/gui/widgets/dialogs/iterate_thickness_dialog.py
@@ -0,0 +1,4 @@
+from src.gui.widgets.dialogs.iteration_dialog import IterationDialog
+
+class ThicknessIterationDialog(IterationDialog):
+    pass
---
+++ src/gui/widgets/dialogs/iterate_tweak_factor_dialog.py
@@ -0,0 +1,4 @@
+from src.gui.widgets.dialogs.iteration_dialog import IterationDialog
+
+class TweakFactorIterationDialog(IterationDialog):
+    pass
