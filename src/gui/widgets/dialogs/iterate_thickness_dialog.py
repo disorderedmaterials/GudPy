649da9b4f6527c22ee0806eb255f586e08730fdb
jswift-stfc <jared.swift@stfc.ac.uk>
2022-04-06 09:53:27 +0100

    feat: don't let thickness iterations be run on a cylindrical sample.

--- src/gui/widgets/dialogs/iterate_thickness_dialog.py
+++ src/gui/widgets/dialogs/iterate_thickness_dialog.py
@@ -1,7 +1,16 @@
 from src.gui.widgets.dialogs.iteration_dialog import IterationDialog
 from src.gudrun_classes.thickness_iterator import ThicknessIterator
+from src.gudrun_classes.enums import Geometry
+from src.gudrun_classes import config
+
 
 class ThicknessIterationDialog(IterationDialog):
+
+    def initComponents(self):
+        super().initComponents()
+        self.widget.iterateButton.setEnabled(
+            config.geometry == Geometry.FLATPLATE
+        )
 
     def iterate(self):
         self.iterator = ThicknessIterator(self.gudrunFile)
