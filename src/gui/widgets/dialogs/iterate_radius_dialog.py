17aaa8a69bcf278c1783fb5f7ab992ec7199d127
jswift-stfc <jared.swift@stfc.ac.uk>
2022-04-06 09:54:07 +0100

    feat: don't let radius iterations be run on a flatplate sample.

--- src/gui/widgets/dialogs/iterate_radius_dialog.py
+++ src/gui/widgets/dialogs/iterate_radius_dialog.py
@@ -1,7 +1,16 @@
 from src.gui.widgets.dialogs.iteration_dialog import IterationDialog
 from src.gudrun_classes.radius_iterator import RadiusIterator
+from src.gudrun_classes.enums import Geometry
+from src.gudrun_classes import config
+
 
 class RadiusIterationDialog(IterationDialog):
+
+    def initComponents(self):
+        super().initComponents()
+        self.widget.iterateButton.setEnabled(
+            config.geometry == Geometry.CYLINDRICAL
+        )
 
     def iterate(self):
         self.iterator = RadiusIterator(self.gudrunFile)
