34f2a9acbc5f043a760ad244b6949ca696a81801
Jared Swift <jared.swift@stfc.ac.uk>
2022-04-19 11:29:38 +0100

    feat: ModEx dialog and event/spectra tables.

---
+++ gudpy/gui/widgets/dialogs/modex_dialog.py
@@ -0,0 +1,39 @@
+import os
+from queue import Queue
+import sys
+
+from PySide6.QtWidgets import QDialog
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
+
+
+class ModexDialog(QDialog):
+
+    def __init__(self, gudrunFile, parent):
+        super(ModexDialog, self).__init__(parent=parent)
+        self.gudrunFile = gudrunFile
+        self.loadUI()
+        self.initComponents()
+
+    def initComponents(self):
+        pass
+
+    def loadUI(self):
+        """
+        Loads the UI file for the PeriodDialog object.
+        """
+        if hasattr(sys, '_MEIPASS'):
+            uifile = QFile(
+                os.path.join(
+                    sys._MEIPASS, "ui_files", "modulationExcitationDialog.ui"
+                )
+            )
+        else:
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            uifile = QFile(
+                os.path.join(
+                    current_dir, "..", "ui_files", "modulationExcitationDialog.ui"
+                )
+            )
+        loader = QUiLoader()
+        self.widget = loader.load(uifile)
---
+++ gudpy/gui/widgets/tables/spectra_table.py
@@ -0,0 +1,132 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QTableView, QDoubleSpinBox
+from src.gui.widgets.tables.gudpy_tables import GudPyTableModel, GudPyDelegate
+
+
+class SpectraModel(GudPyTableModel):
+    """
+    Class to represent a SpectraModel. Inherits GudPyTableModel.
+
+    ...
+
+    Methods
+    -------
+    columnCount(parent)
+        Returns the number of columns in the model.
+    headerData(section, orientation, role)
+        Stub method.
+    setData(index, value, role)
+        Sets data in the model.
+    data(index, role)
+        Returns data at a given index
+    """
+    def __init__(self, data, headers, parent):
+        """
+        Calls super().__init__ on the passed parameters.
+        Parameters
+        ----------
+        data : list
+            Data for model to use.
+        headers: str[]
+            Column headers for table.
+        parent : QWidget
+            Parent widget.
+        """
+        super(SpectraModel, self).__init__(data, headers, parent)
+
+    def columnCount(self, parent):
+        """
+        Returns the number of columns in the model.
+        Parameters
+        ----------
+        parent : QWidget
+            Parent widget.
+        Returns
+        -------
+        int
+            Number of columns in the model - this is always 1.
+        """
+        return 1
+
+    def headerData(self, section, orientation, role):
+        """
+        There are no headers, so this always returns nothing.
+        Returns
+        -------
+        None
+        """
+        pass
+
+    def setData(self, index, value, role):
+        """
+        Sets data in the model.
+        Parameters
+        ----------
+        index : QModelIndex
+            Index to set data at.
+        value : any
+            Value to set data to.
+        role : int
+            Role.
+        """
+        row = index.row()
+        if role == Qt.EditRole:
+            self._data[row] = value
+            self.dataChanged.emit(index, index)
+
+    def data(self, index, role):
+        """
+        Returns the data at a given index.
+        Parameters
+        ----------
+        index : QModelIndex
+            Index to return data from.
+        role : int
+            Role
+        Returns
+        -------
+        float
+            Data at given index.
+        """
+        row = index.row()
+        if role == role & (Qt.DisplayRole | Qt.EditRole):
+            return self._data[row]
+
+
+class SpectraTable(QTableView):
+    """
+    Class to represent a SpectraTable. Inherits QTableView.
+
+    ...
+    Attributes
+    ----------
+    parent : QWidget
+        Parent widget.
+    Methods
+    -------
+    makeModel(data)
+        Creates the model using the data.
+    """
+    def __init__(self, parent):
+        """
+        Constructs all the necessary attributes
+        for the BeamProfileTable object.
+        Calls super().__init__
+        which calls the dunder init method from GudPyTableModel.
+        Parameters
+        ----------
+        parent : QWidget
+            Parent widget.
+        """
+        self.parent = parent
+        super(SpectraTable, self).__init__(parent=parent)
+
+    def makeModel(self, data):
+        """
+        Makes the model and the delegate based on the data.
+        Parameters
+        ----------
+        data : list
+            Data for model to use.
+        """
+        self.setModel(SpectraModel(data, ["Spectra"], self.parent))
