a36db645d48a6668207fced80ea8c91173c351ce
Jared Swift <jared.swift@stfc.ac.uk>
2022-06-06 11:28:28 +0100

    feat: batch processor is working fully in headless mode.

--- gudpy/core/run_batch_files.py
+++ gudpy/core/run_batch_files.py
@@ -6,7 +6,7 @@ from core.tweak_factor_iterator import TweakFactorIterator
 from core.thickness_iterator import ThicknessIterator
 from core.radius_iterator import RadiusIterator
 from core.density_iterator import DensityIterator
-from gudpy.core.gud_file import GudFile
+from core.gud_file import GudFile
 import numpy as np
 
 class BatchProcessor():
@@ -15,99 +15,83 @@ class BatchProcessor():
         self.gudrunFile = gudrunFile
 
     def batch(self, batchSize):
-        samples = {
-            sample : []
-            for sampleBackground in self.gudrunFile.sampleBackgrounds
-            for sample in sampleBackground.samples
-        }
 
-        batches = []
-        for i, sampleBackground in enumerate(self.gudrunFile.sampleBackgrounds):
-            gudrunFile = deepcopy(self.gudrunFile)
-            gudrunFile.sampleBackgrounds = []
+        batch = deepcopy(self.gudrunFile)
+        batch.sampleBackgrounds = []
+        for sampleBackground in self.gudrunFile.sampleBackgrounds:
             batchedSampleBackground = deepcopy(sampleBackground)
             batchedSampleBackground.samples = []
             for sample in sampleBackground.samples:
-                batchedSample = deepcopy(sample)
-                batch = sample.dataFiles[i*batchSize:(i+1)*batchSize]
-                batchedSample.dataFiles.dataFiles = batch
-                batchedSampleBackground.samples.append(batchedSample)
-                samples[sample].append(batchedSample)
-            gudrunFile.sampleBackgrounds.append(batchedSampleBackground)
-            batches.append(gudrunFile)
-        
-        return samples, batches
+                if len(sample.dataFiles) % batchSize == 0:
+                    nBatches = len(sample.dataFiles) // batchSize
+                else:
+                    nBatches = (len(sample.dataFiles) + (len(sample.dataFiles) % batchSize)) // batchSize
+                for i in range(nBatches):
+                    batchedSample = deepcopy(sample)
+                    batchedDataFiles = sample.dataFiles[i*batchSize:(i+1)*batchSize]
+                    batchedSample.dataFiles.dataFiles = batchedDataFiles
+                    batchedSample.name += f"_batch{i}"
+                    batchedSampleBackground.samples.append(batchedSample)
+            batch.sampleBackgrounds.append(batchedSampleBackground)
 
-    def forwardUpdate(self, remappings, batch, iterationMode):
-        if iterationMode == IterationModes.NONE:
-            return
-        for sampleBackground in batch.sampleBackgrounds:
-            for sample in sampleBackground.samples:
-                for ref in remappings.keys():
-                    if ref == sample:
-                        ref.tweakFactor = sample.tweakFactor
-                        ref.upstreamThickness = sample.upstreamThickness
-                        ref.downstreamThickness = sample.downstreamThickness
-                        ref.innerRadius = sample.innerRadius
-                        ref.outerRadius = sample.outerRadius
-                        ref.density = sample.density
-                    for child in remappings[ref]:
-                        child.tweakFactor = sample.tweakFactor
-                        child.upstreamThickness = sample.upstreamThickness
-                        child.downstreamThickness = sample.downstreamThickness
-                        child.innerRadius = sample.innerRadius
-                        child.outerRadius = sample.outerRadius
-                        child.density = sample.density
+        return batch
 
-    def canConverge(self, remappings, rtol):
+    def canConverge(self, batch, rtol):
         if rtol == 0.0:
             return False
-        for sample in remappings.keys():
-            gudPath = sample.dataFiles[0].replace(
-                        self.gudrunFile.instrument.dataFileType,
-                        "gud"
+        for sampleBackground in batch.sampleBackgrounds:
+            for sample in sampleBackground.samples:
+                gudPath = sample.dataFiles[0].replace(
+                            self.gudrunFile.instrument.dataFileType,
+                            "gud"
+                        )
+                gudFile = GudFile(
+                    os.path.join(
+                        self.gudrunFile.instrument.GudrunInputFileDir, gudPath
                     )
-            gudFile = GudFile(
-                os.path.join(
-                    self.gudrunFile.instrument.GudrunInputFileDir, gudPath
                 )
-            )
-            error = round(
-                (
+                error = round(
                     (
-                        gudFile.averageLevelMergedDCS - gudFile.expectedDCS
-                    ) 
-                    / gudFile.averageLevelMergedDCS
-                )*100, 1
-            )
-            if abs(error) > rtol:
-                return False
+                        (
+                            gudFile.averageLevelMergedDCS - gudFile.expectedDCS
+                        ) 
+                        / gudFile.averageLevelMergedDCS
+                    )*100, 1
+                )
+                if abs(error) > rtol:
+                    return False
         return True
 
     def process(self, batchSize=1, headless=True, iterationMode=IterationModes.NONE, rtol=0.0, maxIterations=1):
-        remappings, batches = self.batch(batchSize=batchSize)
-        for i, batch in enumerate(batches):
-            if headless and not self.canConverge(remappings, rtol):
-                if iterationMode == IterationModes.NONE:
-                    batch.process(headless=headless)
-                else:
-                    n = 0
-                    while n < maxIterations:
-                        if iterationMode == IterationModes.TWEAK_FACTOR:
-                            iterator = TweakFactorIterator(batch)
-                        elif iterationMode == IterationModes.THICKNESS:
-                            iterator = ThicknessIterator(batch)
-                        elif iterationMode == IterationModes.INNER_RADIUS:
-                            iterator = RadiusIterator(batch)
-                            iterator.setTargetRadius("inner")
-                        elif iterationMode == IterationModes.OUTER_RADIUS:
-                            iterator = RadiusIterator(batch)
-                            iterator.setTargetRadius("outer")
-                        elif iterationMode == IterationModes.DENSITY:
-                            iterator = DensityIterator(batch)
-                        iterator.performIteration(n)
-                        n+=1
-                        if self.canConverge(remappings, batch, rtol):
-                            break
-                batch.iterativeOrganise(f"batch-{i}")
-                self.forwardUpdate(remappings, iterationMode)
+        batch = self.batch(batchSize=batchSize)
+        print(f"BATCH_PROCESSING: Iteration mode is {iterationMode.name}")
+        print(f"BATCH_PROCESSING: max iterations is {maxIterations}")
+        print(f"BATCH_PROCESSING: rtol for convergence is {rtol}")
+
+        for sample in batch.sampleBackgrounds[0].samples:
+            print(f"BATCH_PROCESSING: starting tweak factor: {sample.sampleTweakFactor}")
+        if headless:
+            n = 0
+            while n < maxIterations:
+                print(f"BATCH_PROCESSING: iteration {n}")
+                if iterationMode == IterationModes.TWEAK_FACTOR:
+                    iterator = TweakFactorIterator(batch)
+                elif iterationMode == IterationModes.THICKNESS:
+                    iterator = ThicknessIterator(batch)
+                elif iterationMode == IterationModes.INNER_RADIUS:
+                    iterator = RadiusIterator(batch)
+                    iterator.setTargetRadius("inner")
+                elif iterationMode == IterationModes.OUTER_RADIUS:
+                    iterator = RadiusIterator(batch)
+                    iterator.setTargetRadius("outer")
+                elif iterationMode == IterationModes.DENSITY:
+                    iterator = DensityIterator(batch)
+                iterator.performIteration(n)
+                batch.process(headless=headless)
+                n+=1
+                if self.canConverge(batch, rtol):
+                    print("BATCH_PROCESSING: convergence tolerance reached.")
+                    break
+            batch.iterativeOrganise(f"BATCH_PROCESSING_BATCH_SIZE_{batchSize}")
+        for sample in batch.sampleBackgrounds[0].samples:
+            print(f"BATCH_PROCESSING: ending tweak factor: {sample.sampleTweakFactor}")
