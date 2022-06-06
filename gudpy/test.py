5876a50c476e6b0f4468d84a031ae075903671ea
Jared Swift <jared.swift@stfc.ac.uk>
2022-06-06 16:55:44 +0100

    refactor: remove test script.

--- gudpy/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from core.run_batch_files import BatchProcessor
-from core.gudrun_file import GudrunFile
-from core.enums import IterationModes
-g = GudrunFile("test/TestData/NIMROD-water/water.txt")
-bproc = BatchProcessor(g)
-bproc.process(batchSize=2, iterationMode=IterationModes.TWEAK_FACTOR, rtol=0.0, maxIterations=5)
