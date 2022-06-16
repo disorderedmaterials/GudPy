bafdf9ea192cebeaad566cdc6c96a50ef3080126
Jared Swift <jared.swift@stfc.ac.uk>
2022-06-16 16:12:16 +0100

    docs: comprehensive commenting for exceptions.

--- gudpy/core/exception.py
+++ gudpy/core/exception.py
@@ -1,6 +1,14 @@
 class ParserException(Exception):
+    """
+    Stub class for ParserException.
+    Raised when errors occur whilst parsing from an input file.
+    """
     pass
 
 
 class ChemicalFormulaParserException(Exception):
+    """
+    Stub class for ChemicalFormulaParserException.
+    Raised when errors occur whilst parsing a chemical formula.
+    """
     pass
