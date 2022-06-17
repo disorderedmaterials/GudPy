3a4581e28b21d75e6429b2251f9ad85717513d8e
Jared Swift <jared.swift@stfc.ac.uk>
2022-06-17 14:44:10 +0100

    docs: comprehensive commenting of RunContainersAsSamples.

--- gudpy/core/run_containers_as_samples.py
+++ gudpy/core/run_containers_as_samples.py
@@ -2,12 +2,38 @@ from copy import deepcopy
 
 
 class RunContainersAsSamples:
+    """
+    Class for running containers as samples.
+    
+    ...
 
+    Attributes
+    ----------
+    gudrunFile : GudrunFile
+        Reference GudrunFile object.
+
+    Methods
+    -------
+    convertContainers()
+        Converts the containers to samples.
+    runContainersAsSamples(path='', headless=False)
+        Runs containers as samples.
+    """
     def __init__(self, gudrunFile):
+        """
+        Sets up the attributes for the RunContainersAsSamples class.
 
+        Paremeters
+        ----------
+        gudrunFile : GudrunFile
+            Reference GudrunFile object.
+        """
         self.gudrunFile = deepcopy(gudrunFile)
 
     def convertContainers(self):
+        """
+        Converts containers to samples.
+        """
         containersAsSamples = []
         for sampleBackground in self.gudrunFile.sampleBackgrounds:
             for sample in sampleBackground.samples:
@@ -18,5 +44,21 @@ class RunContainersAsSamples:
             sampleBackground.samples = containersAsSamples
 
     def runContainersAsSamples(self, path='', headless=False):
+        """
+        Converts containers to samples, and then processes said samples.
+
+        Parameters
+        ----------
+        path : str, optional
+            Path to write to, for Gudrun to use.
+        headless : bool, optional
+            Is his a headless process?
+
+        Returns
+        -------
+        subprocess.CompletedProcess | (QProcess, self.write_out, [path, False])
+            The result of calling gudrun_dcs using subprocess.run, if headless.
+            Otherwise, a QProcess, and intermediate function to call with arguments.        
+        """
         self.convertContainers()
         return self.gudrunFile.dcs(path=path, headless=headless)
