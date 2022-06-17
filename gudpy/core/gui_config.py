0d124d682c9d79d687fa2e3862441aeb15605c1a
Jared Swift <jared.swift@stfc.ac.uk>
2022-06-17 11:08:18 +0100

    docs: comprehensive commenting of GUIConfig.~

--- gudpy/core/gui_config.py
+++ gudpy/core/gui_config.py
@@ -1,4 +1,16 @@
 class GUIConfig():
+    """
+    A simple class for defining the GUI configuration.
+
+    ...
+
+    Attributes
+    ----------
+    useComponents : bool
+        Should components be used?
+    yamlignore : str{}
+        Class attributes to ignore during yaml serialisation.        
+    """
     def __init__(self):
         self.useComponents = False
         self.yamlignore = {
