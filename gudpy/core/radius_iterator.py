3bdf8ce510055e2d04cf5b4a8be236e3caeaea84
Jared Swift <jared.swift@stfc.ac.uk>
2022-06-17 14:37:45 +0100

    docs: comprehensive commenting for radius iterator.

--- gudpy/core/radius_iterator.py
+++ gudpy/core/radius_iterator.py
@@ -10,6 +10,7 @@ class RadiusIterator(SingleParamIterator):
     radius of each sample across iterations.
     The new radii are determined by applying a coefficient calculated
     from the results of gudrun_dcs in the previous iteration.
+
     ...
 
     Methods
@@ -21,14 +22,41 @@ class RadiusIterator(SingleParamIterator):
     organiseOutput
         Organises the output of the iteration.
     """
+
     def applyCoefficientToAttribute(self, object, coefficient):
+        """
+        Applies a computed coefficient the target attribute of the target object.
+
+        Parameters
+        ----------
+        object : Sample
+            Target Sample.
+        coefficient : float
+            Coefficient to apply to target attribute.
+        """
         if self.targetRadius == "inner":
             object.innerRadius *= coefficient
         elif self.targetRadius == "outer":
             object.outerRadius *= coefficient
 
     def setTargetRadius(self, targetRadius):
+        """
+        Sets the targetRadius.
+
+        Parameters
+        ----------
+        targetRadius : str
+            Target Radius to set (inner/outer)
+        """
         self.targetRadius = targetRadius
 
     def organiseOutput(self, n):
+        """
+        Performs iterative organisation on the output.
+
+        Parameters
+        ----------
+        n : int
+            Iteration number
+        """
         self.gudrunFile.iterativeOrganise(f"IterateByRadius_{n}")
