826252970bbb72a4458634e1a5ea1c4c5a8e1460
Noella Spitz <101950441+rhinoella@users.noreply.github.com>
2023-07-21 16:04:32 +0100

    Sorts data files in numerical order (#427)
    * Sorts data files in numerical order
    
    * Changed reverse() to sort() and also added this to 'background data files'

--- gudpy/core/gudrun_file.py
+++ gudpy/core/gudrun_file.py
@@ -613,6 +613,8 @@ class GudrunFile:
             dataFiles = []
             for _ in range(numberOfFiles):
                 dataFiles.append(firstword(self.getNextToken()))
+            # Sorts list so that it is in ascending order
+            dataFiles.sort()
 
             # Create a DataFiles object from the dataFiles list constructed.
             self.normalisation.dataFiles = (
@@ -632,6 +634,9 @@ class GudrunFile:
             dataFilesBg = []
             for j in range(numberOfFilesBg):
                 dataFilesBg.append(firstword(self.getNextToken()))
+
+            # Sorts list so that it is in ascending order
+            dataFilesBg.sort()
 
             # Create a DataFiles object from the dataFiles list constructed.
             self.normalisation.dataFilesBg = (
