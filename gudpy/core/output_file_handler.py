73db138adba0077ee042937fc55e87204202114f
rhinoella <noella.spitz@gmail.com>
2023-10-06 12:29:40 +0100

    Documentation and commenting

--- gudpy/core/output_file_handler.py
+++ gudpy/core/output_file_handler.py
@@ -57,7 +57,16 @@ class OutputFileHandler():
         }
 
     def createNormDir(self, outputDir):
-        # Create normalisation folders
+        """
+        Creates directories for normalisation background
+        and normalisation outputs.
+
+        Parameters
+        ----------
+        outputDir : str
+            Path to target output directory
+        """
+        # Create normalisation folders and move datafiles
         for normFile in self.gudrunFile.normalisation.dataFiles:
             self.copyOutputs(normFile, os.path.join(outputDir, self.NORM))
         for normBgFile in self.gudrunFile.normalisation.dataFilesBg:
@@ -65,8 +74,19 @@ class OutputFileHandler():
                              os.path.join(outputDir, self.NORM_BG))
 
     def createSampleBgDir(self, outputDir):
+        """
+        Creates output directory for sample backgrounds
+
+        Parameters
+        ----------
+        outputDir : str
+            Path to target output directory
+        """
+        # Iterate through all sample backgrounds
         for count, sampleBackground in enumerate(
                 self.gudrunFile.sampleBackgrounds):
+            # Move all datafiles into their sample background folder
+            # Creates the folder if there is none
             for dataFile in sampleBackground.dataFiles:
                 self.copyOutputs(
                     dataFile,
@@ -76,6 +96,21 @@ class OutputFileHandler():
                 )
 
     def createSampleDir(self, outputDir, samples, tree=""):
+        """
+        Creates output directory for each sample
+
+        Parameters
+        ----------
+        outputDir : str
+            Path to target output directory
+        samples : Sample[]
+            List of samples in a sample background
+        tree : str, optional
+            Target basename if running iteratively,
+            is the root folder of the sample folders
+            by default ""
+
+        """
         # Create sample folders within background folders
         for sample in [
             s for s in samples
@@ -91,6 +126,7 @@ class OutputFileHandler():
                     outputDir,
                     sample.name
                 ))
+            # Move datafiles to sample folder
             for dataFile in sample.dataFiles:
                 self.copySuffixedOutputs(
                     dataFile,
@@ -115,9 +151,10 @@ class OutputFileHandler():
                         dataFile,
                         containerPath
                     )
-        return samplePath
 
     def naiveOrganise(self):
+        """Organises Gudrun outputs
+        """
         # Create normalisation and sample background folders
         self.createNormDir(self.outputDir)
         self.createSampleBgDir(self.outputDir)
@@ -128,6 +165,20 @@ class OutputFileHandler():
                 sampleBackground.samples)
 
     def iterativeOrganise(self, nTotal, nCurrent, head):
+        """
+        Organises Gudrun outputs when it is run
+        iteratively
+
+        Parameters
+        ----------
+        nTotal : int
+            Total number of iterations
+        nCurrent : int
+            Current iteration
+        head : str
+            Intended basename for folders
+            which gets incremented per iteration
+        """
         if nCurrent == 0:
             self.createNormDir(self.outputDir)
             self.createSampleBgDir(
@@ -152,7 +203,7 @@ class OutputFileHandler():
 
     def copyOutputs(self, fpath, targetDir):
         """
-        Function to copy all files with the same basename
+        Copy all files with the same basename
         as the provided filepath, except the original file.
         Creates a folder in the target directory and all necessary
         directories.
@@ -181,7 +232,7 @@ class OutputFileHandler():
 
     def copySuffixedOutputs(self, fname, targetDir):
         """
-        Function to copy all files with the same basename
+        Copy all files with the same basename
         as the provided filepath and splits them into outputs
         and diagnostics folders based on their extension.
         Creates a folder in the target directory and all necessary
@@ -196,11 +247,16 @@ class OutputFileHandler():
         targetDir : str
             Directory for the files to be copied to
         """
+        # Data filename
         fname = os.path.splitext(fname)[0]
+        # Path to folder which will hold all outputs from the run
         runDir = os.path.join(targetDir, fname)
+        # Path to folder which will hold Gudrun outputs
         outDir = os.path.join(runDir, self.OUTPUTS)
+        # Path to folder which will hold Gudrun diagnostic outputs
         diagDir = os.path.join(runDir, self.DIAGNOSTICS)
         for f in os.listdir(self.gudrunDir):
+            # Moving all files with output file extensions
             for suffix in self.Outputs["sampleOutputs"]:
                 if f == f"{fname}.{suffix}":
                     if not os.path.isdir(outDir):
@@ -209,6 +265,7 @@ class OutputFileHandler():
                         os.path.join(self.gudrunDir, f),
                         os.path.join(outDir, f)
                     )
+            # Moving all files with diagnostic file extensions
             for suffix in self.Outputs["sampleDiagnostics"]:
                 if f == f"{fname}.{suffix}":
                     if not os.path.isdir(diagDir):
