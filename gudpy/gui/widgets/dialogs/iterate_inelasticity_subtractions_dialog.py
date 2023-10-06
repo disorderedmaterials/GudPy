fec2fb9c7485d8d16ba2ae8971e34e1f8263afe3
rhinoella <noella.spitz@gmail.com>
2023-10-06 11:53:02 +0100

    Modifying dialog functions as needed

--- gudpy/gui/widgets/dialogs/iterate_inelasticity_subtractions_dialog.py
+++ gudpy/gui/widgets/dialogs/iterate_inelasticity_subtractions_dialog.py
@@ -1,3 +1,5 @@
+import os
+from queue import Queue
 from gui.widgets.dialogs.iteration_dialog import IterationDialog
 from core.iterators.wavelength_subtraction_iterator import (
     WavelengthSubtractionIterator
@@ -8,7 +10,17 @@ class WavelengthInelasticitySubtractionsIterationDialog(IterationDialog):
 
     def iterate(self):
         self.iterator = WavelengthSubtractionIterator(self.gudrunFile)
-        self.numberIterations *= 2
         self.enqueueTasks()
         self.text = "Inelasticity subtractions"
         self.widget.close()
+
+    def enqueueTasks(self):
+        self.queue = Queue()
+        for _ in range((self.numberIterations * 2)):
+            self.queue.put(
+                self.iterator.gudrunFile.dcs(
+                    path=os.path.join(
+                        self.gudrunFile.instrument.GudrunInputFileDir,
+                        "gudpy.txt"
+                    ), headless=False)
+            )
