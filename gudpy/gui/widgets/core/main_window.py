2b2522707f834812fe44ed637a217f2f52176f9a
Noella Spitz <noella.spitz@gmail.com>
2023-12-14 10:53:07 +0000

    Removing unecessary organise output

--- gudpy/gui/widgets/core/main_window.py
+++ gudpy/gui/widgets/core/main_window.py
@@ -1538,7 +1538,6 @@ class GudPyMainWindow(QMainWindow):
         if self.error:
             self.procFinished(9, QProcess.NormalExit)
             return
-        self.gudrunFile.gudrunOutput = self.iterator.organiseOutput()
         if self.iterator.nCurrent != -1:
             # If this is not the default run
             self.outputIterations[
