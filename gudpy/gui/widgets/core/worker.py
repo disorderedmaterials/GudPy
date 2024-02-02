583a9fad11c78da9aeb9d1487473c8341b5d00df
Noella Spitz <noella.spitz@gmail.com>
2024-02-02 13:51:27 +0000

    Update workers to thread, add progression functions

--- gudpy/gui/widgets/core/worker.py
+++ gudpy/gui/widgets/core/worker.py
@@ -1,49 +1,147 @@
 import os
+import math
 from copy import deepcopy
+import typing as typ
 from PySide6.QtCore import QObject, Signal, QThread
 
 from core import gudpy
 from core.gud_file import GudFile
+from core.gudrun_file import GudrunFile
+from core.purge_file import PurgeFile
+from core.iterators import Iterator
 from core.sample import Sample
-from core import iterators
+from core import iterators, enums, config, utils
 
 SUFFIX = ".exe" if os.name == "nt" else ""
 
 
-class Worker(QObject):
-    started = Signal(int)
-    errorOccured = Signal(str)
+class GudPyGUI(QObject, gudpy.GudPy):
+    def __init__(
+        self,
+        projectDir: str = "",
+        loadFile: str = "",
+        format: enums.Format = enums.Format.YAML,
+        config: bool = False,
+    ):
+
+        super().__init__(
+            projectDir=projectDir, loadFile=loadFile,
+            format=format, config=config,
+        )
+
+        self.purge = PurgeWorker()
+        self.gudrun = GudrunWorker()
+
+    def processProgress(self, stdout):
+        progress = self.progressIncrementDCS(self.gudrunFile, stdout)
+        if isinstance(self.iterator, iterators.InelasticitySubtraction):
+            progress /= 2
+        progress += self.mainWidget.progressBar.value()
+        self.mainWidget.progressBar.setValue(
+            progress if progress <= 100 else 100
+        )
+
+
+class Worker(QThread):
     outputChanged = Signal(str)
+    progress = Signal(int)
     finished = Signal(int)
 
     def __init___(self):
         super().__init__()
 
     def _outputChanged(self, output):
+        self.output += output
         self.outputChanged.emit(output)
-
-    def _errorOccured(self, stderr):
-        self.errorOccured.emit(stderr)
+        self.progress.emit(self._progress())
 
 
 class PurgeWorker(Worker, gudpy.Purge):
-    def __init__(self, gudrunFile):
-        super().__init__(gudrunFile=gudrunFile)
+    def __init__(self, purgeFile: PurgeFile, gudrunFile: GudrunFile):
+        super().__init__()
+        self.purgeFile = purgeFile
+        self.detectors = None
+        self.dataFiles = [self.gudrunFile.instrument.groupFileName]
 
-    def purge(self):
-        self.started.emit(1)
-        super(PurgeWorker, self).purge()
-        self.finished.emit(1)
+        self.appendDfs(self.gudrunFile.normalisation.dataFiles[0])
+        self.appendDfs(self.gudrunFile.normalisation.dataFilesBg[0])
+        self.appendDfs([df for sb in self.gudrunFile.sampleBackgrounds
+                        for df in sb.dataFiles])
+        if not self.gudrunFile.purgeFile.excludeSampleAndCan:
+            self.appendDfs([df for sb in self.gudrunFile.sampleBackgrounds
+                            for s in sb.samples for df in s.dataFiles
+                            if s.runThisSample])
+            self.appendDfs([df for sb in self.gudrunFile.sampleBackgrounds
+                            for s in sb.samples for c in s.containers
+                            for df in c.dataFiles if s.runThisSample])
+
+    def _progress(self):
+        stepSize = math.ceil(100 / len(self.dataFiles))
+        progress = 0
+        for df in self.dataFiles:
+            if df in self.stdout:
+                progress += stepSize
+        return progress
+
+    def appendDfs(self, dfs):
+        if isinstance(dfs, str):
+            dfs = [dfs]
+        for df in dfs:
+            self.dataFiles.append(
+                df.replace(self.gudrunFile.instrument.dataFileType, "grp")
+            )
+
+    def run(self):
+        exitcode = super(PurgeWorker, self).purge(self.purgeFile)
+        self.finished.emit(exitcode)
+        if exitcode != 0:
+            return
+
+        # Find the number of detectors
+        for line in self.stdout.split("\n"):
+            if "spectra in" in line and self.dataFiles[-1] in line:
+                self.detectors = utils.nthint(line, 0)
 
 
 class GudrunWorker(Worker, gudpy.Gudrun):
-    def __init__(self, gudrunFile, iterator):
-        super().__init__(gudrunFile=gudrunFile, iterator=iterator)
+    def __init__(self, gudrunFile: GudrunFile, iterator: Iterator):
+        super().__init__(iterator=iterator)
+        self.gudrunFile = gudrunFile
 
-    def gudrun(self):
-        self.started.emit(1)
-        super(GudrunWorker, self).gudrun()
-        self.finished.emit(1)
+        # Number of GudPy objects
+        self.markers = (
+            config.NUM_GUDPY_CORE_OBJECTS - 1
+            + len(self.gudrunFile.sampleBackgrounds) + sum([sum([
+                len([
+                    sample
+                    for sample in sampleBackground.samples
+                    if sample.runThisSample
+                ]),
+                *[
+                    len(sample.containers)
+                    for sample in sampleBackground.samples
+                    if sample.runThisSample
+                ]])
+                for sampleBackground in self.gudrunFile.sampleBackgrounds
+            ]))
+
+    def _progress(self):
+        stepSize = math.ceil(100 / self.markers)
+        progress = stepSize * sum([
+            self.stdout.count("Got to: INSTRUMENT"),
+            self.stdout.count("Got to: BEAM"),
+            self.stdout.count("Got to: NORMALISATION"),
+            self.stdout.count("Got to: SAMPLE BACKGROUND"),
+            self.stdout.count("Finished merging data for sample"),
+            self.stdout.count("Got to: CONTAINER"),
+        ])
+        if isinstance(self.iterator, iterators.InelasticitySubtraction):
+            progress /= 2
+        return progress
+
+    def run(self):
+        exitcode = super(GudrunWorker, self).gudrun(self.gudrunFile)
+        self.finished.emit(exitcode)
 
 
 class CompositionWorker(QObject):
