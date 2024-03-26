d11bf200b1d4d18474922f71ad90726efa205251
Noella Spitz <noella.spitz@gmail.com>
2024-03-26 12:29:54 +0000

    Change to using numpy arrays, interpolate

--- gudpy/core/data.py
+++ gudpy/core/data.py
@@ -1,3 +1,4 @@
+import numpy
 
 
 class Point():
@@ -28,31 +29,54 @@ class DataSet():
                 if len(splitLine) > 2:
                     x, y, err, *__ = splitLine
                     if lim and x > lim:
-                        return
+                        return dataSet
                     dataSet.append(Point(x, y, err))
                 else:
                     x, y = splitLine
                     if lim and x > lim:
-                        return
+                        return dataSet
                     dataSet.append(Point(x, y))
 
         return dataSet
 
 
-def calcError(d1: DataSet, d2: DataSet) -> DataSet:
-    err = DataSet("", exists=False)
-    err.dataSet = []
-    for p1, p2 in zip(d1.dataSet, d2.dataSet):
-        errPoint = p1.x - p2.x
-        err.dataSet.append(Point(p1.x, errPoint, 0))
-    return err
+class NpDataSet():
+    # mint01 / mdcs01 / mdor01 / mgor01 / dcs
+    def __init__(self, path, lim=None):
+        self.LIMIT = lim
+        self.x = []
+        self.y = []
+
+        with open(path, "r", encoding="utf-8") as fp:
+            for dataLine in fp.readlines():
+
+                # Ignore commented lines.
+                if dataLine[0] == "#":
+                    continue
+
+                splitLine = [float(n) for n in dataLine.split()]
+                x, y, *__ = splitLine
+                if lim and x > lim:
+                    break
+                self.x.append(x)
+                self.y.append(y)
+
+        if not self.LIMIT:
+            self.LIMIT = self.x[-1]
+
+        self.x = numpy.array(self.x)
+        self.y = numpy.array(self.y)
+
+        self.interpolate()
+
+    def interpolate(self):
+        xnew = numpy.round(numpy.arange(0, self.LIMIT, 0.015), 4)
+        ynew = numpy.interp(xnew, self.x, self.y)
+        self.x = xnew
+        self.y = ynew
 
 
-def meanSquaredError(d1: DataSet, d2: DataSet) -> float:
-    # for p1, p2 in zip(d1.dataSet, d2.dataSet):
-    # print(p1.y, p2.y)
-    # print((p1.y - p2.y)**2)
-
-    return sum((p1.y - p2.y)**2
-               for p1, p2 in zip(d1.dataSet, d2.dataSet)
-               ) / len(d1.dataSet)
+def meanSquaredError(d1: NpDataSet, d2: NpDataSet) -> float:
+    return sum((y1 - y2)**2
+               for y1, y2 in zip(d1.y, d2.y)
+               ) / len(d1.y)
